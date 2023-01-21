230d8209e2d9325d0670ad07a555da7f5f78480a
iobtl <tedmundhtl@gmail.com>
2023-01-21 21:12:15 +0800

    Optional fields in builder struct

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,18 +1,56 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident, Type};
+use syn::{
+    parse_macro_input, Data, DeriveInput, Fields, GenericArgument, Ident, PathArguments, Type,
+};
+
+type IsOption = bool;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
     let struct_name = input.ident;
-    let fields: Vec<(&Ident, &Type)> = match &input.data {
+    let fields: Vec<(&Ident, &Type, IsOption)> = match &input.data {
         Data::Struct(s) => match &s.fields {
             Fields::Named(fields) => fields
                 .named
                 .iter()
-                .filter_map(|f| f.ident.as_ref().map(|ident| (ident, &f.ty)))
+                .filter_map(|f| {
+                    f.ident.as_ref().map(|ident| {
+                        let (ty, is_option) = match &f.ty {
+                            Type::Path(type_path) => {
+                                if type_path.qself.is_none() {
+                                    if let Some(segment) = type_path
+                                        .path
+                                        .segments
+                                        .iter()
+                                        .find(|segment| segment.ident == "Option")
+                                    {
+                                        match &segment.arguments {
+                                            PathArguments::AngleBracketed(args) => {
+                                                // Option must have generic type parameter
+                                                // TODO: Recursive Option definition?
+                                                match args.args.first() {
+                                                    Some(GenericArgument::Type(ty)) => (ty, true),
+                                                    _ => unreachable!(),
+                                                }
+                                            }
+                                            _ => unreachable!(),
+                                        }
+                                    } else {
+                                        (&f.ty, false)
+                                    }
+                                } else {
+                                    (&f.ty, false)
+                                }
+                            }
+                            _ => (&f.ty, false),
+                        };
+
+                        (ident, ty, is_option)
+                    })
+                })
                 .collect(),
             _ => unimplemented!(),
         },
@@ -23,6 +61,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_fields_recurse = fields.iter().map(|f| {
         let name = &f.0;
         let ty = &f.1;
+
         quote! {
             #name: Option<#ty>
         }
@@ -34,8 +73,9 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_fields_none_check_recurse = fields.iter().map(|f| {
         let name = &f.0;
         let error = format!("{} is uninstantiated", name);
+        let is_option = f.2;
         quote! {
-            if self.#name.is_none() {
+            if self.#name.is_none() && !#is_option {
                 return Err(#error.into());
             }
         }
@@ -45,8 +85,16 @@ pub fn derive(input: TokenStream) -> TokenStream {
     };
     let struct_fields_recurse = fields.iter().map(|f| {
         let name = &f.0;
-        quote! {
-            #name: self.#name.take().unwrap()
+        let is_option = f.2;
+
+        if is_option {
+            quote! {
+                #name: self.#name.take()
+            }
+        } else {
+            quote! {
+                #name: self.#name.take().unwrap()
+            }
         }
     });
     let struct_fields = quote! {
