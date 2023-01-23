08f6e7ca87ab49822e9ece59342246114f1c312a
iobtl <tedmundhtl@gmail.com>
2023-01-23 20:46:44 +0800

    Basic implementation of `parse` for custom input

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,41 @@
+use std::ops::Range;
+
 use proc_macro::TokenStream;
+use syn::{
+    parse::{Parse, ParseStream},
+    parse_macro_input, ExprBlock, ExprRange, Ident, LitInt, Token,
+};
+
+#[derive(Debug)]
+struct SeqRange {
+    ident: Ident,
+    range: Range<u16>,
+}
+
+impl Parse for SeqRange {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let ident: Ident = input.parse()?;
+
+        input.parse::<Token![in]>()?;
+
+        let range_start: u16 = input.parse::<LitInt>().and_then(|i| i.base10_parse())?;
+        input.parse::<Token![..]>()?;
+        let range_end: u16 = input.parse::<LitInt>().and_then(|i| i.base10_parse())?;
+
+        input.parse::<ExprBlock>()?;
+
+        Ok(SeqRange {
+            ident,
+            range: range_start..range_end,
+        })
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as SeqRange);
 
-    unimplemented!()
+    eprintln!("{:?}", input);
+
+    TokenStream::new()
 }
