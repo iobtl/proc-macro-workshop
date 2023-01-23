ec3cd8dcf883ce27b95b479c1454b359ec62a6b6
iobtl <tedmundhtl@gmail.com>
2023-01-23 21:17:53 +0800

    Store arbitrary (valid) tokens inside macro input

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,18 +1,21 @@
 use std::ops::Range;
 
 use proc_macro::TokenStream;
+use proc_macro2::{Punct, TokenTree};
+use quote::ToTokens;
 use syn::{
     parse::{Parse, ParseStream},
     parse_macro_input, ExprBlock, ExprRange, Ident, LitInt, Token,
 };
 
 #[derive(Debug)]
-struct SeqRange {
+struct Seq {
     ident: Ident,
     range: Range<u16>,
+    tokens: TokenStream,
 }
 
-impl Parse for SeqRange {
+impl Parse for Seq {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let ident: Ident = input.parse()?;
 
@@ -22,18 +25,25 @@ impl Parse for SeqRange {
         input.parse::<Token![..]>()?;
         let range_end: u16 = input.parse::<LitInt>().and_then(|i| i.base10_parse())?;
 
-        input.parse::<ExprBlock>()?;
+        let block: ExprBlock = input.parse()?;
+        let tokens: TokenStream = block
+            .block
+            .stmts
+            .into_iter()
+            .map(|stmt| TokenStream::from(stmt.into_token_stream()))
+            .collect();
 
-        Ok(SeqRange {
+        Ok(Seq {
             ident,
             range: range_start..range_end,
+            tokens,
         })
     }
 }
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as SeqRange);
+    let input = parse_macro_input!(input as Seq);
 
     eprintln!("{:?}", input);
 
