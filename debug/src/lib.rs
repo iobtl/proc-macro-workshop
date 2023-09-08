29be6cad057d44d485809bb913f1367bbe6611cb
iobtl <tedmundhtl@gmail.com>
2023-09-08 20:10:47 +0800

    Accept debug attribute

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,9 +1,10 @@
 use proc_macro::TokenStream;
-use quote::{quote, ToTokens};
-use syn::{parse_macro_input, Data, DeriveInput};
+use proc_macro2::{Literal, Punct, TokenTree};
+use quote::{quote, quote_spanned, ToTokens};
+use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput};
 use tap::{Pipe, Tap};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     eprintln!("{:#?}", input);
@@ -18,9 +19,54 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let struct_fields_names = struct_fields.iter().map(|field| {
         let name = &field.ident;
         let name_quote = name.as_ref().map(|n| n.to_string());
+        let attrs = &field.attrs;
 
-        quote! {
-            dbg.field(#name_quote, &self.#name);
+        if attrs.is_empty() {
+            quote! {
+                dbg.field(#name_quote, &self.#name);
+            }
+        } else if attrs.len() == 1 {
+            // TODO: clone
+            let mut attr_tokens = attrs.get(0).unwrap().tokens.clone().into_iter();
+
+            // Check for '='
+            match attr_tokens.next() {
+                Some(TokenTree::Punct(punct)) if punct.as_char() == '=' => {}
+                Some(tok) => {
+                    return quote_spanned! {
+                        tok.span() => compile_error!("`debug` attribute should be of the form `debug = \"<format_string>\"`")
+                    };
+                }
+                None => {
+                    return quote_spanned! {
+                        field.span() => compile_error!("`debug` attribute should be of the form `debug = \"<format_string>\"`")
+                    };
+                }
+            }
+
+            // Then, check for format string
+            let format_str = match attr_tokens.next() {
+                // NOTE: don't want this to be a string
+                Some(TokenTree::Literal(lit)) => { lit }
+                Some(tok) => {
+                    return quote_spanned! {
+                        tok.span() => compile_error!("`debug` attribute should be of the form `debug = \"<format_string>\"`")
+                    };
+                }
+                None => {
+                    return quote_spanned! {
+                        field.span() => compile_error!("`debug` attribute should be of the form `debug = \"<format_string>\"`")
+                    }
+                }
+            };
+
+            quote! {
+                dbg.field(#name_quote, &format_args!(#format_str, &self.#name));
+            }
+        } else {
+            quote_spanned! {
+                field.span() => compile_error!("expecting only one inert `debug` attribute")
+            }
         }
     });
     let debug_struct = quote! {
